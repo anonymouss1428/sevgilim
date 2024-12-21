8309fceeab0ae4da6f205e3675e8ae52feb73ded
anonymouss1428 <contralper@gmail.com>
2024-12-21 05:35:27 +0300

    ttasfaasfasashaaasfhgsajkbaskjbfsafassafasfsfsfsfsasafasfassadsssasbjvsaasb

--- askfhaklsjfgsakjfasfogopausıasfahkfasflkahsfuıjags/script.js
+++ askfhaklsjfgsakjfasfogopausıasfahkfasflkahsfuıjags/script.js
@@ -17,32 +17,31 @@ function createSnowflakes() {
 setInterval(createSnowflakes, 100);
 
 // Cevap Kontrol Fonksiyonu
+function checkAnswer(correct, current, next, giftImage = null) {
+    const currentElement = document.getElementById(current);
+    const nextElement = document.getElementById(next);
+    const resultElement = document.getElementById('result');
+    const giftImgElement = document.getElementById('gift-image');
 
- function checkAnswer(correct, current, next, giftImage = null) {
-     const currentElement = document.getElementById(current);
-     const nextElement = document.getElementById(next);
-     const resultElement = document.getElementById('result');
-     const giftImgElement = document.getElementById('gift-image');
+    // Cevap doğruysa
+    const userAnswer = event.target.innerText; // Kullanıcının cevabını al
+    if (userAnswer === correct) {
+        if (giftImage) {
+            // Hediye resmini göster
+            giftImgElement.src = giftImage;
+            resultElement.classList.remove('hidden');
+            currentElement.classList.add('hidden');
 
-     // Cevap doğruysa
-     const userAnswer = event.target.innerText.trim(); // Kullanıcının cevabını al
-     if (userAnswer === correct) {
-         if (giftImage) {
-             // Hediye resmini göster
-             giftImgElement.src = giftImage;
-             resultElement.classList.remove('hidden');
-             currentElement.classList.add('hidden');
-
-             // 2 saniye sonra sonucu gizle ve bir sonraki soruyu göster
-             setTimeout(() => {
-                 resultElement.classList.add('hidden');
-                 if (nextElement) {
-                     nextElement.classList.remove('hidden');
-                 }
-             }, 2000);
-         }
-     } else {
-         // Yanlış cevap verildiğinde
-         alert("Yanlış cevap! Tekrar deneyin.");
-     }
- }
+            // 2 saniye sonra sonucu gizle ve bir sonraki soruyu göster
+            setTimeout(() => {
+                resultElement.classList.add('hidden');
+                if (nextElement) {
+                    nextElement.classList.remove('hidden');
+                }
+            }, 2000);
+        }
+    } else {
+        // Yanlış cevap verildiğinde
+        alert("Yanlış cevap! Tekrar deneyin.");
+    }
+}
