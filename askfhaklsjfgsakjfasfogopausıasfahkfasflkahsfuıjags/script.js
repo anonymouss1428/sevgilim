db9b04569ac70343cc5c2665ddb54ab94a01e97f
anonymouss1428 <contralper@gmail.com>
2024-12-21 04:34:01 +0300

    ttasfaasfasasjkfakljgfdfasaksfs

--- askfhaklsjfgsakjfasfogopausıasfahkfasflkahsfuıjags/script.js
+++ askfhaklsjfgsakjfasfogopausıasfahkfasflkahsfuıjags/script.js
@@ -1,16 +1,6 @@
-function createSnowflakes() {
-    const snowflakeCount = 100; // Kar tanesi sayısı
-    const container = document.body;
-
-    for (let i = 0; i < snowflakeCount; i++) {
-        const snowflake = document.createElement('div');
-        snowflake.classList.add('snowflake');
-        snowflake.innerText = '❄';
-        snowflake.style.left = Math.random() * 100 + 'vw';
-        snowflake.style.fontSize = Math.random() * 10 + 10 + 'px';
-        snowflake.style.animationDuration = Math.random() * 5 + 5 + 's';
-        container.appendChild(snowflake);
+function checkAnswer(correct, current, next) {
+    if (current && next) {
+        document.getElementById(current).classList.add('hidden');
+        document.getElementById(next).classList.remove('hidden');
     }
 }
-
-createSnowflakes();
